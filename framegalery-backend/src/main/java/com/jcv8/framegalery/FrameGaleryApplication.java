69935026a828906a5fa00e033e7dd49b65663a4d
jcvb <github@jcvb.de>
2024-06-13 22:06:28 +0200

    initial commit

---
+++ framegalery-backend/src/main/java/com/jcv8/framegalery/FrameGaleryApplication.java
@@ -0,0 +1,13 @@
+package com.jcv8.framegalery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameGaleryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameGaleryApplication.class, args);
+    }
+
+}
---
+++ framegalery-backend/src/test/java/com/jcv8/framegalery/FrameGaleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcv8.framegalery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameGaleryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
