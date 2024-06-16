5a659b82d9447929821f02a5435289f38c47a3f4
jcvb <github@jcvb.de>
2024-06-16 21:47:40 +0200

    create views & routing

--- framegalery-frontend/src/App.js
+++ framegalery-frontend/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import AuthOutlet from '@auth-kit/react-router/AuthOutlet'
+
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import AdminView from "./pages/admin/AdminView";
+import HomeView from "./pages/HomeView";
+import IngestView from "./pages/admin/IngestView";
+import ImageView from "./pages/ImageView";
+import LoginView from "./pages/admin/LoginView";
+import createStore from 'react-auth-kit/createStore';
+import AuthProvider from "react-auth-kit";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const store = createStore({
+        authName:'_auth',
+        authType:'cookie',
+        cookieDomain: window.location.hostname,
+        cookieSecure: window.location.protocol === 'https:',
+    });
+
+    return (
+        <AuthProvider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={"/"} element={<HomeView/>}></Route>
+                    <Route path={"/image/:id"} element={<ImageView/>}></Route>
+                    <Route path={"/admin/login"} element={<LoginView/>}></Route>
+                    <Route element={<AuthOutlet fallbackPath='/admin/login' />}>
+                        <Route path={"/admin"} element={<AdminView/>}></Route>
+                        <Route path={"/admin/ingest"} element={<IngestView/>}></Route>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
+    );
 }
 
 export default App;
