fd4a58ec27b028e0dc1b6c89ec36d033de40d5bb
JCVB <github@jcvb.de>
2025-03-30 23:54:59 +0200

    remove dev env & maybe fix preference sql error

--- backend/src/main/java/com/jcv8/framegallery/preferences/dataaccess/entity/Preferences.java
+++ backend/src/main/java/com/jcv8/framegallery/preferences/dataaccess/entity/Preferences.java
@@ -17,11 +17,11 @@ public class Preferences {
     private int id;
 
     @ElementCollection
-    @Column(length = 2048)
+    @Column(length = 8192)
     private Map<String, String> preferences;
 
     @ElementCollection
-    @Column(length = 2048)
+    @Column(length = 8192)
     public static final Map<String, String> defaultPreferences = new HashMap<>(
             Map.of(
                     "darkmode", "false",
@@ -31,16 +31,18 @@ public class Preferences {
                     "font-size-title", "1.75rem",
                     "font-size-text", "1.2rem",
                     "font-size-tags", "1rem",
-                    "tracking-code", "  var _paq = window._paq = window._paq || [];\n" +
-                            "  _paq.push(['trackPageView']);\n" +
-                            "  _paq.push(['enableLinkTracking']);\n" +
-                            "  (function() {\n" +
-                            "    var u=\"https://analytics.yourdomain.com/\";\n" +
-                            "    _paq.push(['setTrackerUrl', u+'matomo.php']);\n" +
-                            "    _paq.push(['setSiteId', '0']);\n" +
-                            "    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];\n" +
-                            "    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);\n" +
-                            "  })();"
+                    "tracking-code", """
+                              var _paq = window._paq = window._paq || [];
+                              _paq.push(['trackPageView']);
+                              _paq.push(['enableLinkTracking']);
+                              (function() {
+                                var u="https://analytics.yourdomain.com/";
+                                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                                _paq.push(['setSiteId', '0']);
+                                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                                g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+                              })();\
+                            """
             )
     );
     public Preferences(Map<String, String> preferences) {
